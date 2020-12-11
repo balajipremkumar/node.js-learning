9ed96aa9d206e6e3f24e2f19649874252342f127
balajipremkumar <balajipreamkumar@gmail.com>
2020-12-12 00:51:07 +0530

    basic functionality added

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,55 +1,69 @@
-const yargs = require('yargs')
-const chalk = require('chalk')
-const notes = require('./notes.js');
-// const { command, string } = require('yargs');
+const yargs = require("yargs");
+const chalk = require("chalk");
+const notes = require("./notes.js");
 
 // console.log(process.argv);
+// Customize yargs version
+yargs.version("1.1.0");
 
 //create add command
 yargs.command({
-    command:'add',
-    describe:'Add a new note',
-    builder: {
-        title:{
-            describe:'Note title',
-            demandOption: true,
-            type:'string'
-        },
-        body:{
-            describe:'Note body',
-            demandOption: true,
-            type:'string'
-        }
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
     },
-    handler: function(argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Body: ' + argv.body);
-    }
-})
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
 
 // create remove command
 yargs.command({
-    command:'remove',
-    describe: 'Remove a note',
-    handler: function(){
-        console.log('Removing a notes');
-    }
-})
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    //  console.log("Removing a notes");
+    notes.removeNote(argv.title);
+  },
+});
+
+// create list command
 
 yargs.command({
-    command: 'list',
-    describe:'List a note',
-    handler:function(){
-        console.log('listing out all notes');
-    }
-})
+  command: "list",
+  describe: "List a note",
+  handler() {
+    // console.log("listing out all notes");
+    notes.listNotes();
+  },
+});
+
+// create read command
 
 yargs.command({
-    command:'read',
-    describe:'read a note',
-    handler:function(){
-        console.log('reading a notes');
-    }
-})
+  command: "read",
+  describe: "read a note",
+  handler() {
+    console.log("reading a notes");
+  },
+});
 
 console.log(yargs.argv);
+yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,12 +1,67 @@
-const getNotes = function(){
-    return 'Your Notes....'
-}
+const fs = require("fs");
+const chalk = require("chalk");
 
-const addNote = function(title,body){
+const getNotes = () => {
+  return "Your Notes....";
+};
 
-}
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNote = notes.find((note) => note.title === title);
+
+  if (!duplicateNote) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log(chalk.green.inverse("New note added !"));
+  } else {
+    console.log(chalk.red.inverse("Note title taken !"));
+  }
+};
+
+const removeNote = (title) => {
+  const notes = loadNotes();
+  console.log(notes, "notes");
+  const notesToKeep = notes.filter((note) => note.title !== title);
+  console.log(notesToKeep, "notesToKeep");
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse("Note Removed"));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse("No Note Found"));
+  }
+};
+
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.inverse("Your notes"));
+
+  notes.forEach((note) => {
+    console.log(note.title, note.body);
+  });
+};
+
+const saveNotes = (notes) => {
+  // console.log(notes, "notes");
+  const dataJson = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJson);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJson = dataBuffer.toString();
+    return JSON.parse(dataJson);
+  } catch (e) {
+    return [];
+  }
+};
 
 module.exports = {
-    getNotes:getNotes,
-    addNote:addNote
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
 };
