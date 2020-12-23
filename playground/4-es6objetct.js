399993c2108c7802cddfb2bcf6943ace1cc1d600
balajipremkumar <balajipreamkumar@gmail.com>
2020-12-23 19:38:56 +0530

    object poperty shorthand/ object destructuring

---
+++ playground/4-es6objetct.js
@@ -0,0 +1,32 @@
+// object property shorthand
+const name = "bala";
+const userAge = 29;
+
+const user = {
+  name,
+  age: userAge,
+  location: "chennai",
+};
+
+console.log(user);
+
+// object destructuring
+
+const product = {
+  rate: 100,
+  name: "car",
+  varient: "highend",
+  model: "suv",
+  rating: 4.5,
+};
+
+// const { model: topModel, varient, rating = 5 } = product;
+
+// console.log(topModel, varient);
+// console.log(rating);
+
+const transaction = (type, { varient, model }) => {
+  console.log(type, varient, model);
+};
+
+transaction("order", product);
