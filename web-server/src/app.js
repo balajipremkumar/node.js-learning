acf3b89b76dea39c798e36072d5316558876cc1a
balajipremkumar <balajipreamkumar@gmail.com>
2020-12-25 12:13:24 +0530

    used handelbars,templates with dynamic content.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,7 +8,29 @@ console.log(path.join(__dirname, "../public"));
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "weather App",
+    name: "Andrew Mead",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Andrew Mead",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+    message: "This page will help you with help documents",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
@@ -16,6 +38,7 @@ app.get("/weather", (req, res) => {
     location: "hosur",
   });
 });
+
 app.listen(3000, () => {
   console.log("server is up on port 3000");
 });
