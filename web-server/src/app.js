e62c84e09a8d87f7840a667ce50f4277357ce182
balajipremkumar <balajipreamkumar@gmail.com>
2020-12-30 12:50:45 +0530

    heroku deployment changes, set up app for heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,6 +10,7 @@ console.log(path.join(__dirname, "../public"));
 
 // exprss config
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths ofr Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -89,6 +90,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is up on port 3000");
+app.listen(port, () => {
+  console.log("server is up on port" + port);
 });
